d17f676ea8a438158f826bcc7a638367cb9c6c41
Melvin Yesudas <33954511+melvinyesudas@users.noreply.github.com>
2018-03-17 12:00:09 +0530

    Update lidarospackage.sh

--- Lidar/lidarospackage.sh
+++ Lidar/lidarospackage.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 cd $HOME
-if [ -e "$HOME" ] ; then
-  echo "$DEFAULTDIR already exists; no action taken"
-  echo "Placing Scanse Sweep package into $DEFAULTDIR" 
-else 
-  echo "$DEFAULTDIR does not exist; no action taken."
-  echo "This script expects an existing initialized Catkin Workspace before installing the Scanse Sweep Package."
-  exit 1
-fi
-cd "$DEFAULTDIR"
-echo "Installing prerequisites"
+cd ~/catkin_ws
 source devel/setup.bash
 cd src
 git clone https://github.com/scanse/sweep-ros.git
-cd sweep-ros
-# Install the rosdeps -a = all -y = no questions -r = skip errors 
+cd sweep-ros 
 rosdep install -a -y -r
-cd $HOME
+cd ~/catkin_ws
 catkin_make
